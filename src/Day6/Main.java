22b581a51276f3b6ed4457c92462850afcc3579e
Markus Wizany <9143@htl.rennweg.at>
2022-12-12 08:22:05 +0100

    finished Day 6

--- src/Day6/Main.java
+++ src/Day6/Main.java
@@ -1,7 +1,38 @@
 package Day6;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("src/Day6/input.txt"));
+        String input = lines.get(0);
+
+        String[] list = new String[14];
+        for (int i = 0; i < input.length(); i++) {
+            System.out.println(Arrays.toString(list));
+            if (i < 14) {
+                list[i] = input.charAt(i) + "";
+            } else {
+                boolean yes = true;
+                for (String s : list) {
+                    if (Arrays.stream(list).filter(x -> x.equals(s)).collect(Collectors.toList()).size() > 1) {
+                        yes = false;
+                    }
+                }
+                if (yes) {
+                    System.out.println(i + " | " + Arrays.toString(list));
+                    break;
+                }
+                for (int j = 0; j < 13; j++) {
+                    list[j] = list[j+1];
+                }
+                list[13] = input.charAt(i)+"";
+            }
+        }
     }
 }
